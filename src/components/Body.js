32b4cd6733fd42609428eb495dd448dbc5c0d8c7
Karthik Balachandar <caarthik180@gmail.com>
2024-07-30 02:17:13 +0530

    Path added with react router dom

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,76 @@
-import { useState } from "react";
-import resList from "../utils/mockData";
+import { useEffect, useState } from "react";
 import RestrauntCard from "./RestaurantCard";
+import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
 
 const Body = () => {
-  const [listofRestaurants, setListOfRestaurants] = useState(resList);
-  return (
+  const [listofRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    console.log("useEffect called");
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9469182&lng=80.1480918&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    // Optional Chaning
+    setListOfRestaurants(json?.data?.cards[1]?.card?.card);
+    setFilteredRestaurants(json?.data?.cards[1]?.card?.card);
+  };
+
+  // Ternary condition // Conditional rendering
+  return listofRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      {/* <div className="search">Search</div> */}
       {console.log("listofRestaurants", listofRestaurants)}
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="searchBox"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              const searchedRestaurants =
+                listofRestaurants.gridElements.infoWithStyle.restaurants.filter(
+                  (res) =>
+                    res?.info?.name
+                      .toLowerCase()
+                      .includes(searchText.toLowerCase())
+                );
+              setFilteredRestaurants({
+                ...listofRestaurants,
+                gridElements: {
+                  ...listofRestaurants.gridElements,
+                  infoWithStyle: {
+                    ...listofRestaurants.gridElements.infoWithStyle,
+                    restaurants: searchedRestaurants,
+                  },
+                },
+              });
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           onClick={() => {
             const filteredRestaurants =
               listofRestaurants.gridElements.infoWithStyle.restaurants.filter(
                 (res) => res.info.avgRating > 4.2
               );
-            setListOfRestaurants({
+            setFilteredRestaurants({
               ...listofRestaurants,
               gridElements: {
                 ...listofRestaurants.gridElements,
@@ -32,9 +87,14 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listofRestaurants.gridElements.infoWithStyle.restaurants.map(
+        {filteredRestaurants?.gridElements?.infoWithStyle?.restaurants.map(
           (restaurant) => (
-            <RestrauntCard key={restaurant.info.id} resData={restaurant.info} />
+            <Link
+              key={restaurant.info.id}
+              to={"/restaurants/" + restaurant.info.id}
+            >
+              <RestrauntCard resData={restaurant.info} />
+            </Link>
           )
         )}
       </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [btnNameReact, setBtnNameReact] = useState("Login");
   return (
     <div className="header">
       <div>
@@ -8,10 +11,26 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About Us</Link>
+          </li>
+          <li>
+            <Link to="contact">Contact Us</Link>
+          </li>
           <li>Cart</li>
+          <button
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+            className="login"
+          >
+            {btnNameReact}
+          </button>
         </ul>
       </div>
     </div>
