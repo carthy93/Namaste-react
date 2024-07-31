16f9230730c0845e72e8f0724b17b091774eb817
Karthik Balachandar <caarthik180@gmail.com>
2024-08-01 02:08:12 +0530

    React LifeCycle Methods covered

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,39 @@
-import User from "./User";
+import { Component } from "react";
 import UserClass from "./UserClass";
 
-const About = () => {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <h2>This is Namaste React Web Series</h2>
-      <User name={"Karthik (Function)"} />
-      <UserClass name={"Karthik (Class)"} location={"Chennai (Class)"} />
-    </div>
-  );
-};
+class About extends Component {
+  constructor(props) {
+    super(props);
+    // console.log("Parent constructor");
+  }
+
+  componentDidMount() {
+    // console.log("Parent componentDidMount");
+    // ApI call
+  }
+
+  render() {
+    // console.log("Parent render");
+    return (
+      <div>
+        <h1>About Class Component</h1>
+        <h2>This is Namaste React Web Series</h2>
+        {/* <User name={"Karthik (Function)"} /> */}
+        <UserClass name={"Karthik (Class)"} location={"Chennai (Class)"} />
+      </div>
+    );
+  }
+}
+
+// const About = () => {
+//   return (
+//     <div>
+//       <h1>About Page</h1>
+//       <h2>This is Namaste React Web Series</h2>
+//       {/* <User name={"Karthik (Function)"} /> */}
+//       <UserClass name={"Karthik (Class)"} location={"Chennai (Class)"} />
+//     </div>
+//   );
+// };
 
 export default About;
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,13 +3,44 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    console.log(this.props.name + "Child constructor");
+    this.state = {
+      userInfo: {
+        name: "Dummy",
+        location: "Default",
+        avatar_url: "Dummy",
+      },
+    };
+  }
+
+  async componentDidMount() {
+    console.log(this.props.name + "Child componentDidMount");
+
+    // Api call
+
+    const data = await fetch("https://api.github.com/users/carthy93");
+    const json = await data.json();
+    console.log(json);
+
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentwillunmount");
   }
 
   render() {
-    const { name, location } = this.props;
+    const { name, location, avatar_url } = this.state.userInfo;
+    console.log(name + "Child render");
     return (
       <div className="user-card">
+        <img src={avatar_url} alt="Profile img" />
         <h2>Name: {name}</h2>
         <h3>Location: {location}</h3>
         <h4>Contact: karthikb@gmail.com</h4>
